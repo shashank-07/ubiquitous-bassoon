d4f9a3cdc71e2b6d1402803b5b3fda539207aa7b
shashank-07 <shash07@live.com>
2020-08-30 00:50:43 +0530

    Updates

---
+++ precache-manifest.9ba64b369db51b4863cb0c6b7f633e3d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c910ff81684feef505335fe1ad3f8be7",
+    "url": "/ubiquitous-bassoon/index.html"
+  },
+  {
+    "revision": "1a81643194325d96ae1a",
+    "url": "/ubiquitous-bassoon/static/css/main.1f69820e.chunk.css"
+  },
+  {
+    "revision": "350086573eaa9006e183",
+    "url": "/ubiquitous-bassoon/static/js/2.9a7d51ab.chunk.js"
+  },
+  {
+    "revision": "9434fb467de6e22be11dfea21deb7eb5",
+    "url": "/ubiquitous-bassoon/static/js/2.9a7d51ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a81643194325d96ae1a",
+    "url": "/ubiquitous-bassoon/static/js/main.d706ac8b.chunk.js"
+  },
+  {
+    "revision": "c2bd8600ea9bc3376ed7",
+    "url": "/ubiquitous-bassoon/static/js/runtime-main.7a30da87.js"
+  },
+  {
+    "revision": "8d326706437779ab18fe2a6f97f22679",
+    "url": "/ubiquitous-bassoon/static/media/back_template.8d326706.jpeg"
+  },
+  {
+    "revision": "2aa0f5e0fc65a1e787a420e3cb53977c",
+    "url": "/ubiquitous-bassoon/static/media/backsath.2aa0f5e0.png"
+  },
+  {
+    "revision": "f008c3ec5832246c4586d4f96803b503",
+    "url": "/ubiquitous-bassoon/static/media/gp1_hq.f008c3ec.png"
+  },
+  {
+    "revision": "eb242bb7436270abe27e867d08b2c0d4",
+    "url": "/ubiquitous-bassoon/static/media/sath-1.eb242bb7.png"
+  },
+  {
+    "revision": "3880c594ccc4975e1200a1b44e130b0d",
+    "url": "/ubiquitous-bassoon/static/media/sath-2.3880c594.png"
+  },
+  {
+    "revision": "c1dd3475b733a6f60883c6a20f832172",
+    "url": "/ubiquitous-bassoon/static/media/sath-3.c1dd3475.png"
+  },
+  {
+    "revision": "1ca0a4f6bc67f591e835b54a4af48055",
+    "url": "/ubiquitous-bassoon/static/media/sath-bck-1.1ca0a4f6.JPG"
+  },
+  {
+    "revision": "85b5f5cc2dcd02ae4d6c4732c4e1b77b",
+    "url": "/ubiquitous-bassoon/static/media/sath-bck-2.85b5f5cc.JPG"
+  },
+  {
+    "revision": "ab25a04f7f9c5d707e43626b6b66913e",
+    "url": "/ubiquitous-bassoon/static/media/shashank.ab25a04f.png"
+  }
+]);
