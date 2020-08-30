80a6cb2f97e289e22d4a2ef9822983c09e8a9cf4
shashank-07 <shash07@live.com>
2020-08-30 18:41:11 +0530

    [fix] change rohan background

--- src/static/highlights/rohan.js
+++ src/static/highlights/rohan.js
@@ -2,11 +2,10 @@ import a from '../images/rohan-1.png';
 import b from '../images/rohan-2.png';
 import d from '../images/gp4.png';
 
-import backa from '../images/raj-bck-1.JPG'
-import backb from '../images/raj-bck-2.JPG'
+import backa from '../images/rohan-bck-1.JPG'
 
 
 export const rohan={
     imgs:[a,b,d],
-    background:[backa,backb]
+    background:[backa]
 }
